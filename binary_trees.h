71072dc2ab18409d3fc3347fe44e2fa026c6c347
panaki-gillot <gillotpanaki@gmail.com>
2026-04-09 09:31:15 +0200

    refactor: reorganize binary_tree_t structure definition in header file

--- binary_trees.h
+++ binary_trees.h
@@ -11,7 +11,15 @@
  * @parent: Pointer to the parent node
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
- */
+*/
+
+typedef struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+} binary_tree_t;
 
  binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
  
